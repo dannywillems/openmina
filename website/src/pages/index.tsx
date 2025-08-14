8a621f52d70f88c2248e69393c1259736babbd45
Danny Willems <be.danny.willems@gmail.com>
2025-08-14 22:55:20 +0200

    fixup! Remove openmina naming

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -18,7 +18,7 @@ interface QuickNavItem {
 const quickNavItems: QuickNavItem[] = [
   {
     title: 'Node Runners',
-    description: 'Run and maintain The Rust Node instances',
+    description: 'Run and maintain Rust node instances',
     to: '/docs/node-runners/getting-started',
     icon: (
       <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
